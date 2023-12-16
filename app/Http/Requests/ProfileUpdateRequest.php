ef077cf2e44a7dde929399e8e15181675e115ccd
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2023-12-16 13:53:07 +0330

    complete profile update request

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,6 +17,11 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'alpha_dash:ascii', 'max:20', Rule::unique(User::class)->ignore($this->user()->id)],
+            'title' => ['required', 'string', 'max:40'],
+            'subtitle' => ['nullable', 'string', 'min:5', 'max:225'],
+            'about' => ['nullable', 'string', 'max:255'],
+            'image' => ['nullable', 'image', 'mimes:jpg', 'max:2048'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
     }
