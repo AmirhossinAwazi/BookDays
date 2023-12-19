f88337e164b4b96d3c5d51e0d915f78277c2bc0f
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2023-12-19 16:57:55 +0330

    save new image and delete old image profile; write url by username

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\View\View;
 
 class ProfileController extends Controller
@@ -32,6 +33,14 @@ class ProfileController extends Controller
             $request->user()->email_verified_at = null;
         }
 
+        if ($request->hasFile('image')) {
+            if ($request->user()->image) {
+                Storage::disk('public')->delete($request->user()->image);
+            }
+
+            $filename = $request->user()->username . '.' . $request->file('image')->getClientOriginalExtension();
+            $request->user()->image = $request->file('image')->storeAs('image',$filename,'public');
+        }
         $request->user()->save();
 
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
