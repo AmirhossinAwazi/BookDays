89d8a1cc74e2d199c283c9a785c3a8b9669c3474
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2023-12-24 20:13:03 +0330

    create form for create category

--- resources/views/Admin/category/create.blade.php
+++ resources/views/Admin/category/create.blade.php
@@ -10,7 +10,24 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <x-link-button-gray href="{{ route('category.index') }}">go back</x-link-button-gray>
-                    create categories
+                    
+                    <form action="{{ route('category.store') }}">
+                        @csrf
+                        
+                        <div>
+                            <x-input-label for="title" :value="__('Title')" />
+                            <x-text-input id="title" name="title" type="text" class="mt-1 block w-full" :value="old('title')" required autofocus />
+                            <x-input-error class="mt-2" :messages="$errors->get('title')" />
+                        </div>
+
+                        <div>
+                            <x-input-label for="slug" :value="__('Slug')" />
+                            <x-text-input id="slug" name="slug" type="text" class="mt-1 block w-full" :value="old('slug')" />
+                            <x-input-error class="mt-2" :messages="$errors->get('slug')" />
+                        </div>
+
+                    </form>
+
                 </div>
             </div>
         </div>
