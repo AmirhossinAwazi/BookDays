61e520aa8a72ec4594b0ef2106822c4fe2b25c7c
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2023-12-26 12:36:35 +0330

    put the save button

--- resources/views/Admin/category/create.blade.php
+++ resources/views/Admin/category/create.blade.php
@@ -9,7 +9,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <x-link-button-gray href="{{ route('category.index') }}">go back</x-link-button-gray>
                     
                     <form method="POST" action="{{ route('category.store') }}" class="mt-6 space-y-6">
                         @csrf
@@ -24,6 +23,21 @@
                             <x-input-label for="slug" :value="__('Slug')" />
                             <x-text-input id="slug" name="slug" type="text" class="mt-1 block w-full" :value="old('slug')" />
                             <x-input-error class="mt-2" :messages="$errors->get('slug')" />
+                        </div>
+
+                        <div class="flex items-center gap-4">
+                            <x-link-button-gray href="{{ route('category.index') }}">go back</x-link-button-gray>
+                            <x-primary-button>{{ __('Save') }}</x-primary-button>
+                
+                            @if (session('status') === 'profile-updated')
+                                <p x-data="{ show: true }" 
+                                   x-show="show"
+                                   x-transition
+                                   x-init="setTimeout(() => show = false, 5000)"
+                                   class="text-sm text-gray-600 dark:text-gray-400">
+                                   {{ __('Saved.') }}
+                                </p>
+                            @endif
                         </div>
 
                     </form>
