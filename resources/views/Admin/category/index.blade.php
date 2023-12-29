f3b644c469ce1f9968e738ef9f4aca03bb7614f4
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2023-12-29 20:23:21 +0330

    create delete for category

--- resources/views/Admin/category/index.blade.php
+++ resources/views/Admin/category/index.blade.php
@@ -9,26 +9,38 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <x-link-button-green href="{{ route('category.create') }}">create category</x-link-button-green>
+                    <x-link-button-green href="{{ route('category.create') }}">Create Category</x-link-button-green>
+
                     <div>
                         <x-admin.table>
                             <x-admin.table-header>
-                                <x-admin.table-column>Id</x-admin.table-column>
+                                <x-admin.table-column>ID</x-admin.table-column>
                                 <x-admin.table-column>Title</x-admin.table-column>
                                 <x-admin.table-column>Slug</x-admin.table-column>
                                 <x-admin.table-column></x-admin.table-column>
-
-                                @foreach ($categories as $category)
-                                    <x-admin.table-row>
-                                        <x-admin.table-column>{{ $category->id }}</x-admin.table-column>
-                                        <x-admin.table-column>{{ $category->title }}</x-admin.table-column>
-                                        <x-admin.table-column>{{ $category->slug }}</x-admin.table-column>
-                                        <x-admin.table-column>
-                                            <x-link-button-gray href="{{ route('category.edit', $category) }}">Edit</x-link-button-gray>
-                                        </x-admin.table-column>
-                                    </x-admin.table-row>
-                                @endforeach
                             </x-admin.table-header>
+
+                            @foreach ($categories as $category)
+                                <x-admin.table-row>
+                                    <x-admin.table-column>{{ $category->id }}</x-admin.table-column>
+                                    <x-admin.table-column>{{ $category->title }}</x-admin.table-column>
+                                    <x-admin.table-column>{{ $category->slug }}</x-admin.table-column>
+                                    <x-admin.table-column class="flex gap-2 justify-end">
+                                        <x-link-button-gray href="{{ route('category.edit', $category) }}">
+                                            Edit
+                                        </x-link-button-gray>
+
+                                        <form method="post" action="{{ route('category.destroy', $category) }}">
+                                            @csrf
+                                            @method('delete')
+
+                                            <x-danger-button onclick="return confirm('Are you sure?')">
+                                                Delete
+                                             </x-danger-button>
+                                        </form>
+                                    </x-admin.table-column>
+                                </x-admin.table-row>
+                            @endforeach
                         </x-admin.table>
                     </div>
                 </div>
