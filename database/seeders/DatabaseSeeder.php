207bf095a794f997b1fe6062706d245b67795a4d
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2023-12-30 14:08:54 +0330

    create user in datebas seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'amir hossein',
+            'email' => 'amirhossein@gmail.com',
+            'password' => Hash::make('123456789'),
+            'title' => 'amir hossin',
+            'username' => 'amirhossein',
+        ]);
     }
 }
