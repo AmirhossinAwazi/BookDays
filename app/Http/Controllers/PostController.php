1b48a042134918b2d54247baa6137d68289c30dc
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-01-04 20:04:36 +0330

    create update and edit for post in post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Tag;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -65,17 +67,39 @@ class PostController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Request $request, Post $post)
     {
-        //
+        return view('admin.post.edit', [
+            'categories' => $request->user()->categories,
+            'post' => $post,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdatePostRequest $request, Post $post)
     {
-        //
+        if ($request->hasFile('image')) {
+            Storage::disk('public')->delete($post->image);
+
+            $post->image = $request->file('image')->store('post-image', 'public');
+        }
+
+        $post->fill([
+            'title' => $request->input('title'),
+            'slug' => $request->input('slug') ?? Str::slug($request->input('title')),
+            'body' => $request->input('body'),
+            'category_id' => $request->input('category_id'),
+            'published_at' => $request->input('published_at'),
+            'is_draft' => $request->boolean('is_draft') ?? false,
+        ]);
+
+        $post->save();
+
+        $post->tags()->sync(Tag::findOrCreateFromRequest($request));
+
+        return to_route('post.index');
     }
 
     /**
