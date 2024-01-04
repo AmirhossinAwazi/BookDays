383410fd29360ba36090a21efa246cc8693a9cd7
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-01-04 19:45:10 +0330

    clear the code

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
@@ -16,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/',[BlogController::class, 'index'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -28,5 +27,3 @@ Route::middleware('auth')->group(function () {
     Route::resource('/post',PostController::class);
     Route::resource('/category',CategoryController::class);
 });
-
-require __DIR__.'/auth.php';
