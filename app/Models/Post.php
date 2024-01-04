dd2d33bc5be9f166d5bd69e7547ed56f4293a84c
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-01-04 20:02:38 +0330

    create relation for tags, imageurl in post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
@@ -19,7 +21,27 @@ class Post extends Model
         'image',
     ];
 
-    public function category(){
-        return $this->belongsTo(category::class);
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function imageUrl(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => Storage::disk('public')->url($this->image),
+        );
+    }
+
+    public function tagsString(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => implode(', ', $this->tags()->pluck('name')->toArray()),
+        );
     }
 }
