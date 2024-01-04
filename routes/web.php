1808164a7fd75bdb5239f7133d6fcc5221f9752e
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-01-04 20:31:23 +0330

    clear route web code

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('/post',PostController::class);
-    Route::resource('/category',CategoryController::class);
+    Route::resource('/post',PostController::class)->except(['show']);
+    Route::resource('/category',CategoryController::class)->except(['show']);
 });
