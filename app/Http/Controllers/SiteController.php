2423951554ab8f549ee6cce12875748454e366d9
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-01-06 13:42:35 +0330

    get the latestposts

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class SiteController extends Controller
 {
     public function index ()
     {
-        return view('welcome');
+        
+        $latestPosts = Post::latest()->take(1)->get();
+        return view('welcome',[
+         'latestPosts' => $latestPosts,
+        ]);
+
+        
     }
 }
