c6906266b6c2d61ca413c211a3475cf656bc47a3
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-01-11 16:25:31 +0330

    edited

--- resources/views/components/home/section.blade.php
+++ resources/views/components/home/section.blade.php
@@ -1,7 +1,7 @@
 @props(['title'])
 
 <div class="container mx-auto">
-    <h3 class="mt-48 text-5xl text-gray-500 font-semibold">{{ $title }}</h3>
+    <h3 class="mt-10 text-5xl text-gray-500 font-semibold">{{ $title }}</h3>
     <hr class="mt-6 w-32 border-2 rounded border-orange-400">
 
     {{ $slot }}
