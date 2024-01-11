2ef03bd84e35779f85da7aabc4748e54d1507c94
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-01-11 13:07:06 +0330

    create components for latestpost in welcome

--- resources/views/components/widgets/post-card.blade.php
+++ resources/views/components/widgets/post-card.blade.php
@@ -1,18 +1,18 @@
 @props(['post'])
 
-<a href="#">
-    <img src="{{ $post->image_url }}" alt="{{ $post->title }}"">
+<a href="#" class="flex-1 rounded-3xl drop-shadow-md hover:drop-shadow-2xl overflow-hidden">
+    <img src="{{ $post->image_url }}" alt="{{ $post->title }}" class="h-64 w-full object-cover">
 
-    <div>
-        <div>
+    <div class="p-6 bg-white">
+        <div class="px-3 py-1 rounded-md inline-flex bg-orange-100 text-orange-400 font-semibold">
             {{ $post->author->title }}
         </div>
 
-        <div>
+        <div class="mt-2 text-xl font-semibold">
             {{ $post->title }}
         </div>
 
-        <div>
+        <div class="mt-2 text-sm text-gray-400 font-semibold">
             {{ $post->created_at->diffForHumans() }}
         </div>
     </div>
