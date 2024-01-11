d52de47c0003d663a55d4538c20c5a42fe44b507
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-01-11 16:34:47 +0330

    edited

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -12,7 +12,14 @@ class SiteController extends Controller
     {
         
         $latestPosts = Post::latest()->take(3)->get();
-        $latestBlogs = User::take(3)->get();
+        $latestBlogs = User::orderBy(
+            Post::select('created_at')
+                ->whereColumn('users.id', 'posts.author_id')
+                ->latest()
+                ->take(1)
+        , 'desc')
+        ->take(3)->get();
+
         return view('welcome',[
          'latestPosts' => $latestPosts,
          'latestBlogs' => $latestBlogs,
