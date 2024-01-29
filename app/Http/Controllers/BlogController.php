a29f4ed17f4a457e79ce4cc09ebf41f3abd03c30
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-01-29 17:12:03 +0330

    create route domain and blog index

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
+    public function index(User $user)
+    {
+        $post = $user->posts()->paginate(5);
+        $categories = $user->categories;
 
+        return view('blog.index', [
+
+            'posts' => $post,
+            'categories' => $categories,
+
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,14 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SiteController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::domain('{user:username}.bookdays.test')->group(function() {
+    Route::get('/', function($user) {
+        dd($user);
+    });
+});
 
 Route::get('/',[SiteController::class, 'index'])->name('home');
 
