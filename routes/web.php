67b8f26f43478f82d1d6bec702da31501baa5bbd
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-01-31 16:24:43 +0330

    create route domain

--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,23 @@ use App\Http\Controllers\BlogController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\SiteController;
-use App\Models\User;
+use App\Http\Controllers\SiteController;    
 use Illuminate\Support\Facades\Route;
 
-Route::domain('{user:username}.bookdays.test')->group(function() {
-    Route::get('/', function($user) {
-        dd($user);
-    });
+
+Route::domain('{user:username}.bookdays.test')->group(function () {
+   Route::get('/', [BlogController::class, 'index']);
 });
 
-Route::get('/',[SiteController::class, 'index'])->name('home');
+
+Route::get('/', [SiteController::class, 'index'])->name('home');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('/post',PostController::class)->except(['show']);
-    Route::resource('/category',CategoryController::class)->except(['show']);
+    Route::resource('/post', PostController::class)->except(['show']);
+    Route::resource('/category', CategoryController::class)->except(['show']);
 });
