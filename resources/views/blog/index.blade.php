0230f37f9b5839275693a97c8b70fe884b32289f
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-02-03 17:31:49 +0330

    clear code

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,37 +1,36 @@
 <x-blog-layout>
 
+    <div class="bg-primary-200">
+        <div class="flex container mx-auto justify-around items-center">
+            <div class="font-title text-6xl text-primary-900">{{ $blog->title }}</div>
+            <div class="text-xl text-primary-500">{{ $blog->subtitle }}</div>
+            <div class="h-96">
+                <img src="{{ $blog->image_url }}" alt="{{ $blog->title }}" class="w-full h-full object-cover rounded-3xl relative top-16">
+            </div>
+        </div>
+    </div>
+
     <div class="mt-32 p-16 container text-justify mx-auto space-y-32">
         @foreach ($posts as $post)
-            <div class="flex gap-32">
-                <div class="flex-1">
-                    <div class="text-lg text-gray-400 font-semibold">{{ $post->created_at->diffForhumans() }}</div>
-                    <div class="font-bold text-5xl">{{ $post->title }}</div>
-                    <div class="mt-4 px-3 py-0.5 rounded-md inline-flex bg-orange-100 text-orange-400 font-semibold">{{ $post->category->title }}</div> 
-                    @foreach ($post->tags as $tag)
-                        <div class="mt-4 px-3 py-0.5 rounded-md inline-flex bg-sky-100 text-sky-400 font-semibold">{{ $tag->name }}</div>  
-                    @endforeach
-                    <div class="mt-4 text-xl text-gray-500">{!! str($post->body)->words(50, '...') !!}</div> 
-                    <div class=" mt-10">
-                        <a href="#" class="mt-4 px-3 py-1.5 text-xl rounded-lg font-semibold bg-lime-300 text-lime-800 hover:bg-lime-400 hover:text-lime-900">Read More</a>                        
-                    </div>
-                </div>
-                <div  class="flex-1">
-                    <img src="{{ $post->image_Url }}" alt="{{ $post->title }}" class="h-96 w-full object-cover rounded-3xl">
-                </div>
-            </div>
+        <x-widgets.post-item :$blog :$post/>
         @endforeach
-        {{ $posts->links() }}
     </div>
 
     <x-slot name="footer">
-        <div class="flex px-12 text-justify gap-16">
-            <div class="text-xl font-semibold">About</div>
-            <div class="flex flex-col items-start gap-4">
+        <div class="grid grid-cols-3 px-12 text-justify gap-16">
+
+            <div class="text-left">
+                <h3 class="text-xl font-semibold">About</h3>
+                <div>{{ $blog->about }}</div>
+            </div>
+
+            <div class="flex flex-col items-start gap-1">
                 <h3 class="text-xl font-semibold">Categories</h3>
                 @foreach ($categories as $category)
-                    <a href="#">{{ $category->title }}</a>
+                    <a href="#" class=" text-primary-700 hover:text-primary-900">{{ $category->title }}</a>
                 @endforeach
             </div>
+
         </div>
     </x-slot>
 
