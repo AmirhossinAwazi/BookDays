c8ef39811e0222f82586f0e8a63f2edbf52222aa
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-02-04 18:21:03 +0330

    get the tags

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -9,12 +10,16 @@ class BlogController extends Controller
 {
     public function index(User $user)
     {
-        $post = $user->posts()->with(['category', 'tags'])->paginate(5);
+        $posts = $user->posts()->with(['category', 'tags'])->paginate(5);
         $categories = $user->categories;
+        $tags = Tag::WhereHas('posts', function($query) use($user) {
+            $query->where('author_id', $user->id);
+        })->get();
 
         return view('blog.index', [
             'blog' => $user,
-            'posts' => $post,
+            'posts' => $posts,
+            'tags' => $tags,
             'categories' => $categories,
 
         ]);
