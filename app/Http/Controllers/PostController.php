84b4d3aed420a812fff9bbd1c8e30c45243ec3d1
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-02-05 16:00:35 +0330

    give user to postcontroller for find subdomain

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function show(Post $post)
+    public function show(User $user, Post $post)
     {
         dd($post);
     }
