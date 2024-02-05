794585b00d871fc830c66680cae557de031e3724
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-02-05 14:17:22 +0330

    add the route post

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::domain('{user:username}.' . config('app.url'))->group(function () {
    Route::get('/', [BlogController::class, 'index'])->name('blog.index');
+   Route::get('/post/{post}', [BlogController::class, 'show'])->name('blog.show');
    Route::post('/subscriber', [SubscriberController::class, 'store'])->name('subscriber.store');
 });
 
