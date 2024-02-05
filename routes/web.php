bc8f448d92a906d9ab4d4515ea2e0006cb92bfc2
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-02-05 14:51:11 +0330

    change the controllers of the post to postcontroller

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\SiteController;
 use App\Http\Controllers\SubscriberController;
 use Illuminate\Support\Facades\Route;
 
 Route::domain('{user:username}.' . config('app.url'))->group(function () {
    Route::get('/', [BlogController::class, 'index'])->name('blog.index');
-   Route::get('/post/{post}', [BlogController::class, 'show'])->name('blog.show');
+   Route::get('/post/{post}', [PostController::class, 'show'])->name('blog.show');
    Route::post('/subscriber', [SubscriberController::class, 'store'])->name('subscriber.store');
 });
 
