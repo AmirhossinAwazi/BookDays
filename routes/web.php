e790ab7b1a959a2c439833102c80c337d75352c7
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-02-05 15:54:41 +0330

    change the name to post.show

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::domain('{user:username}.' . config('app.url'))->group(function () {
    Route::get('/', [BlogController::class, 'index'])->name('blog.index');
-   Route::get('/post/{post}', [PostController::class, 'show'])->name('blog.show');
+   Route::get('/post/{post}', [PostController::class, 'show'])->name('post.show');
    Route::post('/subscriber', [SubscriberController::class, 'store'])->name('subscriber.store');
 });
 
