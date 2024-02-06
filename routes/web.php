e9a87941126cdf588ea8d84ade334ccf9a472949
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-02-06 13:46:29 +0330

    add the comment route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\SiteController;
 use App\Http\Controllers\SubscriberController;
@@ -10,6 +11,7 @@ Route::domain('{user:username}.' . config('app.url'))->group(function () {
    Route::get('/', [BlogController::class, 'index'])->name('blog.index');
    Route::get('/post/{post:slug}', [PostController::class, 'show'])->name('post.show');
    Route::post('/subscriber', [SubscriberController::class, 'store'])->name('subscriber.store');
+   route::post('/post/{post:slug}/comment', [CommentController::class, 'store'])->name('comment.store');
 });
 
 Route::get('/', [SiteController::class, 'index'])->name('home');
