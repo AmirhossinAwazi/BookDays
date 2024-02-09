ca99b908adcb60c92154d91a557cb5b975746590
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-02-09 17:03:44 +0330

    create store comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCommentRequest;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function store(StoreCommentRequest $Request, User $user,Post $post)
+    {
+        $post->comments()->create($Request->validated());
+
+        return back()
+            ->with('comment-created', 'Your comment awaits moderation. Thanks!')
+            ->withFragment('comments');
+    }
 }
