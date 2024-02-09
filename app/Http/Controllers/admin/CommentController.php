b40d59f581c1e53c5695520142e6fcdfc8b65e18
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-02-09 20:33:59 +0330

    complite update comment controller admin

--- app/Http/Controllers/admin/CommentController.php
+++ app/Http/Controllers/admin/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateCommentRequest;
 use App\Models\Comment;
 use Illuminate\Http\Request;
 
@@ -20,5 +21,12 @@ class CommentController extends Controller
         return view('Admin.comment.edit', [
             'comment' => $comment,
         ]);
+    }
+
+    public function update(UpdateCommentRequest $request, Comment $comment)
+    {
+        $comment->update($request->validated());
+
+        return to_route('comment.index');
     }
 }
