e767729a3dcf1c2c5b9b856e66fd4adee9575f3c
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-02-09 21:57:50 +0330

    use the comment model in comment controller

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreCommentRequest;
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function store(StoreCommentRequest $Request, User $user,Post $post)
+    public function store(StoreCommentRequest $Request, User $user,Post $post, Comment $comment)
     {
         $post->comments()->create($Request->validated());
 
