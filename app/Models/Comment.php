233a6d13d0812ee36fd784c384cff7a94afe5668
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-02-11 22:51:49 +0330

    complit the relation belongsto

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
@@ -25,7 +27,7 @@ class Comment extends Model
         static::addGlobalScope('latest', fn ($q) => $q->latest());
     }
 
-    public function post()
+    public function post(): BelongsTo
     {
         return $this->belongsTo(Post::class);
     }
