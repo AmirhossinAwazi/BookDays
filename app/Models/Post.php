7f77b7b9554f96b1add12abffabcc611a9342313
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-02-11 22:52:34 +0330

    complit the relation hasmany

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
@@ -36,7 +37,7 @@ class Post extends Model
         return $this->belongsToMany(Tag::class);
     }
 
-    public function comments()
+    public function comments(): HasMany
     {
         return $this->hasMany(Comment::class);
     }
