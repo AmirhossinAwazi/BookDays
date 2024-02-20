f3bdc65544364ce3a723deaff2f2fe1da6895bf0
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-02-20 18:12:45 +0330

    create notification for newcomment mail

---
+++ app/Notifications/NewCommentNotification.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewCommentNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('You have a new comment')
+            ->line('A new comment has been posted on you blog.')
+            ->line('Head to your dashboard to moderate the comment.')
+            ->action('Go to Dashboard', route('comment.index'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
