ecc4666d118e4959d9356d82bdfebf1547c6df54
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-02-25 19:00:46 +0330

    conect the new post notif to mail

--- app/Listeners/NotifySubscribersAboutNewPost.php
+++ app/Listeners/NotifySubscribersAboutNewPost.php
@@ -3,8 +3,10 @@
 namespace App\Listeners;
 
 use App\Events\NewPost;
+use App\Mail\NewPostMail;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
 
 class NotifySubscribersAboutNewPost
 {
@@ -21,6 +23,8 @@ class NotifySubscribersAboutNewPost
      */
     public function handle(NewPost $event): void
     {
-        dd('im in the listener');
+        $event->post->author->subscribers->each(function ($subscriber) use ($event) {
+            Mail::to($subscriber->email)->send(new NewPostMail($event->post));
+        });;
     }
 }
